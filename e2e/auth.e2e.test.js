e1bcc16ec5267657eb4cdba65b6096f1eaef8c96
David Morgan <david.morgan@europe.com>
2022-03-03 19:40:17 +0000

    End-to-end tests

---
+++ e2e/auth.e2e.test.js
@@ -0,0 +1,120 @@
+const { Pool } = require('pg');
+const bcrypt = require('bcrypt');
+const request = require('supertest');
+const app = require('../server');
+
+// Create a new pool with a connection limit of 1
+const mockPool = new Pool({
+	database: 'ecom',
+	user: 'postgres',
+	password: 'postgres',
+	port: 5432,
+	max: 1, // Reuse the connection to make sure we always hit the same temporal schema
+	idleTimeoutMillis: 0, // Disable auto-disconnection of idle clients to make sure we always hit the same temporal schema
+});
+
+jest.mock('../db/index', () => {
+	return {
+		async query(text, params) {
+			return await mockPool.query(text, params);
+		},
+
+		async getClient() {
+			return await mockPool.connect();
+		},
+	};
+});
+
+afterAll(async () => {
+	await mockPool.end();
+});
+
+beforeEach(async () => {
+	await mockPool.query('CREATE TEMPORARY TABLE account (LIKE account INCLUDING ALL)');
+});
+
+afterEach(async () => {
+	await mockPool.query('DROP TABLE IF EXISTS pg_temp.account');
+});
+
+describe('/auth/login', () => {
+	describe('post', () => {
+		it('Allows user to log in', async () => {
+			const passwordHash = await bcrypt.hash('Password01', 10);
+			const values = [1, 'David', 'Morgan', 'david.morgan@gmail.com', passwordHash];
+			await mockPool.query('INSERT INTO account (id, first_name, last_name, email, password_hash) VALUES ($1, $2, $3, $4, $5)', values);
+
+			await request(app)
+				.post('/auth/login')
+				.send({
+					email: 'david.morgan@gmail.com',
+					password: 'Password01'
+				})
+				.expect(200);
+		});
+
+		it('Rejects login with incorrect email', async () => {
+			const passwordHash = await bcrypt.hash('Password01', 10);
+			const values = [1, 'David', 'Morgan', 'david.morgan@gmail.com', passwordHash];
+			await mockPool.query('INSERT INTO account (id, first_name, last_name, email, password_hash) VALUES ($1, $2, $3, $4, $5)', values);
+
+			await request(app)
+				.post('/auth/login')
+				.send({
+					email: 'david.morgan@mail.com',
+					password: 'Password01'
+				})
+				.expect(401);
+		});
+
+		it('Rejects login with incorrect password', async () => {
+			const passwordHash = await bcrypt.hash('Password01', 10);
+			const values = [1, 'David', 'Morgan', 'david.morgan@gmail.com', passwordHash];
+			await mockPool.query('INSERT INTO account (id, first_name, last_name, email, password_hash) VALUES ($1, $2, $3, $4, $5)', values);
+
+			await request(app)
+				.post('/auth/login')
+				.send({
+					email: 'david.morgan@gmail.com',
+					password: 'Password1'
+				})
+				.expect(401);
+		});
+	});
+});
+
+describe('/auth/register', () => {
+	describe('post', () => {
+		it('Allows a user to register a new account', async () => {
+			await request(app)
+				.post('/auth/register')
+				.send({
+					firstName: 'David',
+					lastName: 'Morgan',
+					email: 'david.morgan@gmail.com',
+					password: 'Password01',
+				})
+				.expect(200, {
+					firstName: 'David',
+					lastName: 'Morgan',
+					email: 'david.morgan@gmail.com',
+				});
+		});
+
+		it('Does not allow a user to register a new account with an existing email address', async () => {
+			const passwordHash = await bcrypt.hash('Password01', 10);
+			const values = [1, 'David', 'Morgan', 'david.morgan@gmail.com', passwordHash];
+			await mockPool.query('INSERT INTO account (id, first_name, last_name, email, password_hash) VALUES ($1, $2, $3, $4, $5)', values);
+
+			await request(app)
+				.post('/auth/register')
+				.send({
+					firstName: 'David',
+					lastName: 'Morgan',
+					email: 'david.morgan@gmail.com',
+					password: 'Password01',
+				})
+				.expect(500);
+		});
+	});
+});
