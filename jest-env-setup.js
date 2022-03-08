403482ae59cf5fbaa0add4bea8213ea06e621805
David Morgan <david.morgan@europe.com>
2022-03-08 16:44:22 +0000

    Parameterize postgres settings

--- jest-env-setup.js
+++ jest-env-setup.js
@@ -1,2 +1,7 @@
-// Create the express session secret for the test environment
+// Create environment variables the test environment
 process.env.SESSION_SECRET = 'my mock secret';
+process.env.PG_USER = 'postgres';
+process.env.PG_PASSWORD = 'postgres';
+process.env.PG_HOST = 'localhost';
+process.env.PG_DATABASE = 'ecom';
+process.env.PG_PORT = 5432;
