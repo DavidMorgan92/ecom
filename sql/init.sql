d8001f731df4751804053dbc6d101dabf7208793
David Morgan <david.morgan@europe.com>
2022-05-30 14:38:20 +0100

    Seed file for database

---
+++ sql/init.sql
@@ -0,0 +1,11 @@
+INSERT INTO "product" ("name", "description", "category", "price_pennies", "stock_count")
+VALUES ('Toothbrush', 'Bristly', 'Health & Beauty', 123, 12),
+('Hairbrush', 'For your head', 'Health & Beauty', 321, 32),
+('TV', 'State of the art OLED', 'Electronics', 100000, 10),
+('Phone', 'The latest Android phone', 'Electronics', 50000, 50),
+('Tinned Tuna', 'Delishy fishy', 'Food', 1000, 100),
+('Frozen Burgers', 'Tasty burgers', 'Food', 500, 50),
+('Dinner Plate Set', '8 stylish dinner plates', 'Kitchenware', 1500, 15),
+('Knife Set', 'Sharp and durable', 'Kitchenware', 500, 50),
+('Leather Chair', 'Comfy', 'Furniture', 5000, 5),
+('Desk', 'Big and woody', 'Furniture', 12000, 12);
